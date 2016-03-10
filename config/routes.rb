0b641a83d46d5db375efcc284ab78c3f126b53d5
stevenchentw <sadz9999@hotmail.com>
2016-03-10 12:32:47 +0800

    finish CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+match ':controller(/:action(/:id(.:format)))', :via => :all
 end
