14fecac74ca374e35f82d59c8eb711da81b46736
stevenchentw <sadz9999@hotmail.com>
2016-03-10 20:54:48 +0800

    1.RESTful 2.respond_to

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,25 @@ class EventsController < ApplicationController
   before_action :set_event, :only => [ :show, :edit, :update, :destroy]
    
   def index
-  	@events = Event.page(params[:page]).per(5)
+  	@events = Event.page(params[:page]).per(10)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events.to_xml }
+      format.json { render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } # index.atom.builder
+    end
+
   end
 
   def show
     @page_title = @event.name
+
+    respond_to do |format|
+      format.html { @page_title = @event.name } # show.html.erb
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   def new
@@ -21,7 +35,7 @@ class EventsController < ApplicationController
 
       flash[:notice] = "新增成功"
 
-  	  redirect_to :action => :index   
+  	  redirect_to events_path
     else
       render :action => :new
     end
@@ -37,7 +51,7 @@ class EventsController < ApplicationController
 
       flash[:notice] = "編輯成功"
 
-      redirect_to :action => :show , :id => @event
+      redirect_to event_path(@event)
     else
       # redirect_to :action => :edit , :id => @event
       render :action => :edit
@@ -49,7 +63,7 @@ class EventsController < ApplicationController
 
      flash[:alert] = "刪除成功"
 
-     redirect_to :action => :index
+     redirect_to events_path
   end
 
 
